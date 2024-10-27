bca58df3e1688a8a6c85c64f96099d9499982034
Odd Egil Hegge Selnes <post@odde.org>
2024-10-27 16:02:05 +0100

    installed Jest and configured ESLint to ignore test-files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,23 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 
+import globals from "globals";
+
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["**/*.test.js", "**/__tests__/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
 ];
